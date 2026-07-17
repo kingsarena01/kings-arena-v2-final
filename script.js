de91a9b4b2a24423d1afa7a3ab71ae745df47c93
kingsarena01 <thuraesport01@gmail.com>
2026-07-17 19:10:40 +0630

    Implement tournament loading from Firestore
    Added functionality to load tournaments from Firestore.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 import { db } from "./firebase-config.js";
-
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 console.log("King's Arena Website Loaded");
 console.log("Group Draw Loaded");
+const tournamentSelect = document.getElementById("tournamentSelect");
+async function loadTournaments() {
+    const snapshot = await getDocs(collection(db, "tournaments"));
+    console.log(snapshot);
+}
+
+loadTournaments();
