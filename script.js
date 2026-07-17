a9155402552bedb252eee241f3929d63f069e877
kingsarena01 <thuraesport01@gmail.com>
2026-07-18 00:08:56 +0630

    test alert

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 console.log("King's Arena Website Loaded");
-
+alert("Script Loaded");
 const tournamentSelect = document.getElementById("tournamentSelect");
 
 async function loadTournaments() {
